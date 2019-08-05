19aeba9b3b2ad6a1d50f96542931df416c52b8dc
liliyaik <limma@yandex.ru>
2019-08-05 22:35:28 +0300

    Добавлены отступы

--- sendbox/src/main/java/ru/stqa/lil/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/lil/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.lil.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args)
-{
-System.out.println("Hello? world!");
-}
+  public static void main(String[] args)
+  {
+    System.out.println("Hello? world!");
+  }
 }

277c9ab4f82a66d8e1d81ffca05dfa39d692e18d
liliyaik <limma@yandex.ru>
2019-08-11 19:53:10 +0300

    Задание 4 Добавление нового контакта

---
+++ addressbook_web_tests/src/test/java/ru/qa/pft/addressbook/CreatedNewContact.java
@@ -0,0 +1,85 @@
+package ru.qa.pft.addressbook;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CreatedNewContact {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    login("admin", "secret");
+  }
+
+  private void login(String name, String password) {
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(name);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys( password);
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testAddContact() throws Exception {
+    gotoContactpage("add new");
+    fillDataForm(new GroupDataContact("LILIYA", "IKSANOVA", "limma", "Moscow", "limma@yandex.ru"));
+    SubmitCreateContact();
+    logout("Logout");
+  }
+
+  private void logout(String logout) {
+    wd.findElement(By.linkText(logout)).click();
+  }
+
+  public void SubmitCreateContact() {
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+  }
+
+  public void fillDataForm(GroupDataContact groupDataContact) {
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(groupDataContact.getName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(groupDataContact.getFio());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(groupDataContact.getNik());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(groupDataContact.getCity());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(groupDataContact.getEmail());
+  }
+
+  public void gotoContactpage(String s) {
+    wd.findElement(By.linkText(s)).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
---
+++ addressbook_web_tests/src/test/java/ru/qa/pft/addressbook/GroupDataContact.java
@@ -0,0 +1,37 @@
+package ru.qa.pft.addressbook;
+
+public class GroupDataContact {
+  private final String name;
+  private final String fio;
+  private final String nik;
+  private final String city;
+  private final String email;
+
+  public GroupDataContact(String name, String fio, String nik, String city, String email) {
+    this.name = name;
+    this.fio = fio;
+    this.nik = nik;
+    this.city = city;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFio() {
+    return fio;
+  }
+
+  public String getNik() {
+    return nik;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}

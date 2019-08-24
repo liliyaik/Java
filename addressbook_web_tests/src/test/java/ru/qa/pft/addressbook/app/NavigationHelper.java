899f8c1413e7942f88c716023535629af872864e
liliyaik <limma@yandex.ru>
2019-08-24 12:49:00 +0300

    3.9 Добавлены проверки для групп

--- addressbook_web_tests/src/test/java/ru/qa/pft/addressbook/app/NavigationHelper.java
+++ addressbook_web_tests/src/test/java/ru/qa/pft/addressbook/app/NavigationHelper.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.security.acl.Group;
+
 public class NavigationHelper extends BaseHelper {
 
 
@@ -16,6 +18,20 @@ public class NavigationHelper extends BaseHelper {
    *
    */
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+      && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+      && isElementPresent(By.name("new")))
+    {
+      return;
+    }
     click(By.linkText("groups"));
+  }
+
+  public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable")))
+    {
+      return;
+    }
+    click(By.linkText("home"));
   }
 }

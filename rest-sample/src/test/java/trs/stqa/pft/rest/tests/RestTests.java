b6705cfddf5162013fe5b65f1ca2f3566cb61db9
liliyaik <limma@yandex.ru>
2019-10-03 20:57:26 +0300

    SOAP, REST samples

---
+++ rest-sample/src/test/java/trs/stqa/pft/rest/tests/RestTests.java
@@ -0,0 +1,28 @@
+package trs.stqa.pft.rest.tests;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTests extends TestBase {
+
+
+  @Test(enabled = true)
+  public void testCreateIssue() throws IOException {
+
+    int isId = 50;
+   // skipIfNotFixed(isId);
+
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+    int issueId = createIssue(newIssue);
+    System.out.println("Created issue " + issueId);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+}
